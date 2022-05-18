32709e779bf7f3d41811d5afd560407f1e52a647
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-05-18 16:11:07 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import App from '../src/views/index.js';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Icons from './../../assets/icons/boltz_logo.png';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
